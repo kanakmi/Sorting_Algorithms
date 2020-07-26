6bbe68d28401bc04bd6f6e67babf8d0b71aaa5bf
kanakmi <54859521+kanakmi@users.noreply.github.com>
2020-07-26 11:41:11 +0530

    Quick Sort Algorithm

--- quicksort.cpp
+++ quicksort.cpp
@@ -32,8 +32,10 @@ int main(){
     int n;
     cin >> n;
     int i, a[n];
-    for(i=0; i<n; i++) cin>>a[i];
+    for(i=0; i<n; i++)
+        cin>>a[i];
     quicksort(a, 0, n-1);
-    for(i=0; i<n; i++) cout<<a[i]<<' ';
+    for(i=0; i<n; i++)
+        cout<<a[i]<<' ';
     return 0;
 }
